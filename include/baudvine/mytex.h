e97767c35d2bc105f94073df9faf1728afdcbd90
Dominic van Berkel <dominic@baudvine.net>
2022-09-03 12:01:34 +0200

    first shot at TryLock

--- include/baudvine/mytex.h
+++ include/baudvine/mytex.h
@@ -1,13 +1,12 @@
 #pragma once
 
 #include <mutex>
+#include <optional>
 
 namespace baudvine {
-
 template <typename T, typename Lock>
 class MytexGuard {
  public:
-  template <typename L = Lock>
   MytexGuard(T* object, Lock lock) : object_(object), lock_(std::move(lock)) {}
 
   T& operator*() noexcept { return *object_; }
@@ -20,18 +19,52 @@ class MytexGuard {
   Lock lock_;
 };
 
-template <typename T, typename Mutex = std::mutex>
+/// @brief Same as \c MytexGuard, except it might be empty.
+template <typename T, typename Lock>
+class OptionalMytexGuard {
+public:
+  OptionalMytexGuard() = default;
+  OptionalMytexGuard(T* object, Lock lock)
+      : data_(Data{object, std::move(lock)}) {}
+
+  [[nodiscard]] bool has_value() const noexcept { return data_.has_value(); }
+  T& value() { return *data_.value().object; }
+  const T& value() const { return *data_.value().object; }
+
+  T& operator*() noexcept { return *data_->object; }
+  T const& operator*() const noexcept { return *data_->object; }
+  T* operator->() noexcept { return &**this; }
+  T const* operator->() const noexcept { return &**this; }
+
+ private:
+  struct Data {
+    T* object;
+    Lock lock;
+  };
+  std::optional<Data> data_{};
+};
+
+template <typename T, typename Lockable = std::mutex>
 class Mytex {
  public:
+  using LockT = std::unique_lock<Lockable>;
+
   template <typename... Args>
   Mytex(Args&&... initialize) : object_(std::forward<Args>(initialize)...) {}
 
-  MytexGuard<T, std::unique_lock<Mutex>> Lock() {
-    return {&object_, std::unique_lock<Mutex>(mutex_)};
+  MytexGuard<T, LockT> Lock() { return {&object_, LockT(mutex_)}; }
+
+  OptionalMytexGuard<T, LockT> TryLock() {
+    LockT lock(mutex_, std::try_to_lock);
+    if (lock.owns_lock()) {
+      return {&object_, std::move(lock)};
+    } else {
+      return {};
+    }
   }
 
  private:
   T object_;
-  Mutex mutex_;
+  Lockable mutex_;
 };
 }  // namespace baudvine
--- test/test_mytex.cpp
+++ test/test_mytex.cpp
@@ -2,9 +2,27 @@
 
 #include <gtest/gtest.h>
 
+#include <thread>
+
 TEST(Mytex, BasicCreateLockDestroy) {
-  baudvine::Mytex<int> underTest(5); 
+  baudvine::Mytex<int> underTest(5);
   EXPECT_EQ(*underTest.Lock(), 5);
   *underTest.Lock() = 6;
   EXPECT_EQ(*underTest.Lock(), 6);
+}
+
+TEST(Mytex, TryLock) {
+  baudvine::Mytex<int> underTest(6);
+  {
+    auto number = underTest.TryLock();
+    EXPECT_TRUE(number.has_value());
+    EXPECT_EQ(number.value(), 6);
+    EXPECT_EQ(*number, 6);
+
+    EXPECT_FALSE(underTest.TryLock().has_value());
+    std::thread([&underTest] {
+      EXPECT_FALSE(underTest.TryLock().has_value());
+    }).join();
+  }
+  EXPECT_TRUE(underTest.TryLock().has_value());
 }
