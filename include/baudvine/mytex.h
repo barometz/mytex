4c5ddec843c774c6320d6ae01f7955dd6b353a2e
Dominic van Berkel <dominic@baudvine.net>
2022-09-03 12:37:03 +0200

    Add detail namespace and some comments

--- include/baudvine/mytex.h
+++ include/baudvine/mytex.h
@@ -4,6 +4,13 @@
 #include <optional>
 
 namespace baudvine {
+namespace detail {
+/** @brief A lock_guard-a-like that includes a reference to the guarded
+ * resource.
+ *
+ * \c MytexGuard holds the lock it was created with until it goes out of scope.
+ * As long as you hold it, you have exclusive access to the referenced resource.
+ */
 template <typename T, typename Lock>
 class MytexGuard {
  public:
@@ -19,15 +26,22 @@ class MytexGuard {
   Lock lock_;
 };
 
-/// @brief Same as \c MytexGuard, except it might be empty.
+/** @brief Same as \c MytexGuard, except it might be empty.
+ *
+ * Without this, \c Mytex::TryLock() would return \c std::optional<MytexGuard>
+ * which introduces a second layer of indirection. \c OptionalMytexGuard
+ * integrates \c std::optional, exposes some of its functionality, and lets you
+ * get at the optionally-contained value with one dereference.
+ */
 template <typename T, typename Lock>
 class OptionalMytexGuard {
-public:
+ public:
   OptionalMytexGuard() = default;
   OptionalMytexGuard(T* object, Lock lock)
       : data_(Data{object, std::move(lock)}) {}
 
   [[nodiscard]] bool has_value() const noexcept { return data_.has_value(); }
+  [[nodiscard]] explicit operator bool() const noexcept { return has_value(); }
   T& value() { return *data_.value().object; }
   const T& value() const { return *data_.value().object; }
 
@@ -43,18 +57,23 @@ public:
   };
   std::optional<Data> data_{};
 };
+}  // namespace detail
 
+/** @brief A mutex that owns the resource it guards.
+ */
 template <typename T, typename Lockable = std::mutex>
 class Mytex {
  public:
   using LockT = std::unique_lock<Lockable>;
+  using Guard = detail::MytexGuard<T, LockT>;
+  using OptionalGuard = detail::OptionalMytexGuard<T, LockT>;
 
   template <typename... Args>
   Mytex(Args&&... initialize) : object_(std::forward<Args>(initialize)...) {}
 
-  MytexGuard<T, LockT> Lock() { return {&object_, LockT(mutex_)}; }
+  Guard Lock() { return {&object_, LockT(mutex_)}; }
 
-  OptionalMytexGuard<T, LockT> TryLock() {
+  OptionalGuard TryLock() {
     LockT lock(mutex_, std::try_to_lock);
     if (lock.owns_lock()) {
       return {&object_, std::move(lock)};
